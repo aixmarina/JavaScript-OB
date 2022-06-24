33abb051825002432ed9a71d9f5a95597b6d1d68
aixmarina <marina.aix.gomez@gmail.com>
2022-06-24 21:06:52 +0300

    terminado ejercicio 2

---
+++ Ejercicio2/index.js
@@ -0,0 +1,19 @@
+/*Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+- Tu nombre (string)
+- Tu edad (number)
+- ¿Eres desarrollador? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)*/
+
+const lista = [
+    "Marina del Carmen Aix Gómez",
+    19,
+    true,
+    new Date(2002, 6, 16),
+    {
+        titulo: "43 maneras de soltarse el pelo",
+        autor: "Elvira Sastre",
+        fecha: new Date(2014, 4, 6),
+        url: "https://www.amazon.es/Cuarenta-tres-maneras-soltarse-pelo/dp/8415786298"
+    },
+];
